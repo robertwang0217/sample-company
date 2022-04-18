1e2c28953e83d26ef01f9fbe2da4ccbce9564852
Robert Wang <robertwang0217@gmail.com>
2022-04-18 21:27:55 +1000

    create Company and Employee model as required

---
+++ database/migrations/2022_04_18_111535_create_employees_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('first_name')->nullable(false);
+            $table->string('last_name')->nullable(false);
+
+            $table->integer('company_id')->unsigned();
+
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::table('employees', function (Blueprint $table) {
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
